5e76d02585f517e2df84305d5fd48514cc80cf9c
KoumaTeacup <shingleex@gmail.com>
2020-07-18 15:27:18 -0700

    added missing extension

--- public/javascripts/context.js
+++ public/javascripts/context.js
@@ -1,6 +1,10 @@
 var canvas = document.querySelector("#c");
 var gl = canvas.getContext("webgl2", { preserveDrawingBuffer: true });
-var ext = gl.getExtension('EXT_color_buffer_float');
+var EXT_color_buffer_float = gl.getExtension('EXT_color_buffer_float');
+var OES_texture_float_linear = gl.getExtension('OES_texture_float_linear');
+if (!OES_texture_float_linear || !EXT_color_buffer_float) {
+  alert('missing WebGL extension support, please use the latest browser!');
+}
 if (!gl) {
   throw new Error('WebGL failed to initialize');
 }
--- public/javascripts/framebuffer.js
+++ public/javascripts/framebuffer.js
@@ -20,6 +20,7 @@ class Framebuffer {
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, this.width, this.height, 0, gl.RGBA, gl.FLOAT, null);
 
     // no mip & clamp to edge
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
