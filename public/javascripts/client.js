00adf42b60382f7517eadab9ddd2c3ff111bdff1
Shin Chen <schen@insomniacgames.com>
2022-01-27 15:29:14 -0800

    added bare bone insomniac power up

---
+++ public/javascripts/client.js
@@ -0,0 +1,27 @@
+/* global TrelloPowerUp */
+
+var Promise = TrelloPowerUp.Promise;
+
+var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+TrelloPowerUp.initialize({
+    'board-buttons': function (t, opts) {
+        return [{
+            // we can either provide a button that has a callback function
+            icon: {
+                dark: WHITE_ICON,
+                light: BLACK_ICON
+            },
+            text: 'Callback',
+            callback: onBtnClick,
+            condition: 'edit'
+        }];
+    }
+});
+
+var onBtnClick = function (t, opts) {
+    console.log('Insomniac Trello Power-Up Under construction');
+};
