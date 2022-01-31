ec754ed0a4c56c5c841b16069e818eccd6efeeaf
KoumaTeacup <shingleex@gmail.com>
2022-01-31 00:31:27 -0800

    added like image

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,20 +7,40 @@ var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
 TrelloPowerUp.initialize({
-    'board-buttons': function (t, opts) {
-        return [{
-            // we can either provide a button that has a callback function
-            icon: {
-                dark: WHITE_ICON,
-                light: BLACK_ICON
-            },
-            text: 'IP Garden Feature Center',
-            callback: onBtnClick,
-            condition: 'edit'
-        }];
-    }
+  'board-buttons': function (t, opts) {
+    return [{
+      // we can either provide a button that has a callback function
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'IP Garden Feature Center',
+      callback: onBtnClick,
+      condition: 'edit'
+    }];
+  },
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+        return [
+          {
+            // It's best to use static badges unless you need your
+            // badges to refresh.
+            // You can mix and match between static and dynamic
+            text: "Vote",
+            icon: "IG_Garden_like.png", // for card front badges only
+            color: green,
+          },
+        ];
+      });
+  },
+
 });
 
 var onBtnClick = function (t, opts) {
-    alert('Insomniac Trello Power-Up Under construction');
+  alert('Insomniac Trello Power-Up Under construction');
 };
